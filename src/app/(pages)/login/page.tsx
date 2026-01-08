a0f09fe1cf9a7e16b44db08cbb48631af8d8aad6
SGGStudios <shanejoubert12@gmail.com>
2026-01-08 11:49:56 +0200

    added lib

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function Login(){
+    return <div>
+            <h1>Login form</h1>
+            <div>
+                
+            </div>
+            <div className="flex gap-6">
+                <button className="bg-green-200 p-2 rounded-lg">Login</button>
+                <Link href="/" className="bg-gray-200 p-2 rounded-lg">Cancel</Link>
+            </div>
+        </div>
+}
